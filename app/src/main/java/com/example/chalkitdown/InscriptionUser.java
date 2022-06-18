a27bc37dd534971f081f589f2afa4bf5fc07cdcb
soupapoule <62351063+soupapoule@users.noreply.github.com>
2022-06-18 23:13:58 +0400

    1st

---
+++ app/src/main/java/com/example/chalkitdown/InscriptionUser.java
@@ -0,0 +1,18 @@
+package com.example.chalkitdown;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InscriptionUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscriptionuser);
+
+
+    }
+
+}
